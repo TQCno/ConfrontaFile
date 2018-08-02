296b8f26e6e15ac06eef1312ac8f15d630ed30d1
Davide <Davide@DAVIDE-PC>
2018-08-02 21:01:32 +0200

    Check Color

--- ImageControl/ImageControl/Funzioni.cs
+++ ImageControl/ImageControl/Funzioni.cs
@@ -3,10 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Drawing;
 
 namespace ImageControl
 {
     class Funzioni
     {
+        public bool isInside(Bitmap palette, Color color)
+        {
+            int sizeW = palette.Width;
+            int sizeH = palette.Height;
+
+            for (int i = 0; i < sizeH; i++)
+            {
+                for (int j = 0; j < sizeW; j++)
+                {
+                    if (palette.GetPixel(j, i) == color) return true;
+                }
+            }
+
+            return false;
+        }
+
     }
+    
 }
